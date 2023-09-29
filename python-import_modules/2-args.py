c5e31a21d91a6575d88c3be05eb559c7f5d720ef
GreyLeaf23 <christian.torres2894@gmail.com>
2023-09-29 15:16:22 -0400

    Program prints number and list of arguments.

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,8 +2,8 @@
 if __name__ == "__main__":
     import sys
 
-arguments = sys.argv[1:]
-num_arguments = len(arguments)
+argument = len(sys.argv) - 1
+num_arguments = argument
 
 if num_arguments == 0:
         print("0 arguments.")
@@ -12,7 +12,7 @@ elif num_arguments == 1:
      print("1 argument:")
 
 else:
-    print("{}".format(num_arguments))
+    print("{} arguments:".format(num_arguments))
 
 for i in range(num_arguments):
     print("{}: {}".format(i + 1, sys.argv[i + 1]))
