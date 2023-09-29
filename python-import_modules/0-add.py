a4933a02071bf2223cf3cc0758b2995874c39619
GreyLeaf23 <christian.torres2894@gmail.com>
2023-09-29 12:38:51 -0400

    Modify file.

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-#Importing function from 'add_0.py' file/module.
 if __name__ == "__main__":
-
+#Importing function from 'add_0.py' file/module.
     from add_0 import add
 
     a = 1
     b = 2
-
     result = add(a, b)
+
     print("{} + {} = {}".format(a, b, result))
