480000deea3201a322de395acd27f3bd74e23b8c
GreyLeaf23 <christian.torres2894@gmail.com>
2023-09-29 12:29:26 -0400

    Add "if" statement

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 #Importing function from 'add_0.py' file/module.
-from add_0 import add
+if __name__ is "__main__":
 
-a = 1
-b = 2
+    from add_0 import add
 
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+    a = 1
+    b = 2
+
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
