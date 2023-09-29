fa9bfeb49ae08ed85cc7212042b839fc129bcdc2
GreyLeaf23 <christian.torres2894@gmail.com>
2023-09-29 15:17:28 -0400

    Pycodestyle fix.

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,14 +6,13 @@ argument = len(sys.argv) - 1
 num_arguments = argument
 
 if num_arguments == 0:
-        print("0 arguments.")
+    print("0 arguments.")
 
 elif num_arguments == 1:
-     print("1 argument:")
+    print("1 argument:")
 
 else:
     print("{} arguments:".format(num_arguments))
 
 for i in range(num_arguments):
     print("{}: {}".format(i + 1, sys.argv[i + 1]))
-
