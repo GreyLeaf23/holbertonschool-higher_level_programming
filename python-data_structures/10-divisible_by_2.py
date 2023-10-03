ac8ce77d82c74b8e6a5480dac8c9b96e72ceca55
GreyLeaf23 <christian.torres2894@gmail.com>
2023-10-03 18:25:34 -0400

    Function finds numbers divisible by 2.

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    new_list = []
+
+    for num in range(len(my_list)):
+        if my_list[num] % 2 == 0:
+            new_list.append(True)
+        else:
+            new_list.append(False)
+
+    return new_list
