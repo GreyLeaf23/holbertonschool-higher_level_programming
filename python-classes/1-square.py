7ea93512457268d9f4e7b2436de91748bfa3d7a8
GreyLeaf23 <christian.torres2894@gmail.com>
2023-10-10 11:04:46 -0400

    Class 'Square' define as a square.

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defining a class named Square"""
+
+
+class Square:
+    """Square"""
+
+    def __init__(self, size):
+        """Start new square"""
+        self.__size = size
