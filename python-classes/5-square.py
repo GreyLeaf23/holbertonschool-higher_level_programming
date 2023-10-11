abb2b52d18072d81348acddeb3e76c7ab8c872a2
GreyLeaf23 <christian.torres2894@gmail.com>
2023-10-11 18:51:54 -0400

    Print squares in the form of '#'.

---
+++ python-classes/5-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""Defining a class named Square"""
+
+
+class Square:
+    """Square"""
+
+    def __init__(self, size=0):
+        """Start new square"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = size
+
+
+    def area(self):
+        """Define square area"""
+        return self.__size ** 2
+
+
+    @property
+    def size(self):
+        """Get square size"""
+        return self.__size
+
+
+    @size.setter
+    def size(self, value):
+        """Set square size"""
+
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+
+        else:
+            self.__size = value
+
+
+    def my_print(self):
+        """Print square"""
+
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                print("#" * self.__size)
