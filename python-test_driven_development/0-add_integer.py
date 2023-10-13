e1b1ec2545494b44ac513216a5426eb37067b420
GreyLeaf23 <christian.torres2894@gmail.com>
2023-10-12 20:39:34 -0400

    Various files.

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """Add two integers"""
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
