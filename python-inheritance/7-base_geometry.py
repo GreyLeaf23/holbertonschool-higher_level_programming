ac28b76421d3e58406f70bdcba2200992eb7c6a6
GreyLeaf23 <christian.torres2894@gmail.com>
2023-10-18 18:43:21 -0400

    Method validates if value is an integer, along with test-docu.

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module contains a class named BaseGeometry"""
+
+
+class BaseGeometry:
+    """Creates a class named BaseGeometry"""
+
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
