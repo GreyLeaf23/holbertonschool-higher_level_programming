22a20fa48b83d7d9424a8afd543cb69a302985ed
GreyLeaf23 <christian.torres2894@gmail.com>
2023-10-18 18:17:55 -0400

    Module defines a method that raises an exception.

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module contains a class named BaseGeometry"""
+
+
+class BaseGeometry:
+    """Creates a class named BaseGeometry"""
+
+    def area(self):
+        """Raises an exception"""
+        raise Exception("area() is not implemented")
