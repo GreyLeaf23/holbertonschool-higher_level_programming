59d851291fabbe05e543b30065360fcda8480cd7
GreyLeaf23 <christian.torres2894@gmail.com>
2023-10-18 21:09:43 -0400

    Module creates class named Rectangle.

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -7,13 +7,20 @@ from BaseGeometry class.
 
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
-    """Creates a class named Rectangle, which inherits
-    from BaseGeometry class.
+    """inherits from BaseGeometry
+    Methods:
+        __init__(self, width, height)
     """
+
     def __init__(self, width, height):
-        """Initializes an instance"""
-        self.integer_validator("width", width)
+        """validate and initialize width and height
+        Args:
+            width (int): private
+            height (int): private
+        """
+        super().integer_validator("width", width)
         self.__width = width
-        self.integer_validator("height", height)
+        super().integer_validator("height", height)
         self.__height = height
