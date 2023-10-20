18c2136e72611e6b77f1afdad38846e87bc5cf4b
GreyLeaf23 <christian.torres2894@gmail.com>
2023-10-19 21:15:04 -0400

    Module implements area and prints rectangle.

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" This module have class called Rectangle that inherits from BaseGeometry"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Class Rectangle that inherits from BaseGeometry"""
+    def __init__(self, width, height):
+        """ Function that validates the weight & height """
+        self.__width = width
+        self.__height = height
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+
+    def area(self):
+        """Returns the area """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns the following rectangle description"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
