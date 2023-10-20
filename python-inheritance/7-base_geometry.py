bbeb3de38b270e2cdc09e42fe3cccd0ef995d400
GreyLeaf23 <christian.torres2894@gmail.com>
2023-10-19 20:12:56 -0400

    Tweak Task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -13,5 +13,5 @@ class BaseGeometry:
         """Validates value"""
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
