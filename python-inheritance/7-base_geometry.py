a62b1b3d3864da52b3afaaf793035e3414fafcbc
GreyLeaf23 <christian.torres2894@gmail.com>
2023-10-19 20:23:54 -0400

    Tweak Task 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
-"""Module contains a class named BaseGeometry"""
+"""
+Defines a BaseGeometry class.
+"""
 
 
 class BaseGeometry:
-    """Creates a class named BaseGeometry"""
+    """
+    Defines a BaseGeometry class.
+    """
 
     def area(self):
-        """Raises an exception"""
+        """
+        Raises an Exception with the message "area() is not implemented".
+        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """Validates value"""
+        """
+        Validates value.
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
+        if value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        else:
-            self.name = name
-            self.value = value
