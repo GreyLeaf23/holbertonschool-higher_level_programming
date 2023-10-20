14741e1b94a0bc42501814d6767cc185f893097c
GreyLeaf23 <christian.torres2894@gmail.com>
2023-10-20 11:47:50 -0400

    Module converts JSON string to Python object.

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module that returns an object (Python data structure) represented by a JSON"""
+
+
+def from_json_string(my_str):
+    """Returns converted object from JSON string."""
+    import json
+    return json.loads(my_str)
