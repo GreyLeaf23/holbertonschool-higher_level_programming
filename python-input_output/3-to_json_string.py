a7d027fca328d1bc294f3c2e88d9ab9c707301b3
GreyLeaf23 <christian.torres2894@gmail.com>
2023-10-20 11:35:33 -0400

    Tweak Task 3.

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,8 +2,7 @@
 """Module that returns the JSON representation of an object (string)."""
 
 
-import json
-
 def to_json_string(my_obj):
     """Returns the JSON representation of an object (string)."""
+    import json
     return json.dumps(my_obj)
