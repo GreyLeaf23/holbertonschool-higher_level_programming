0c63cc27744d9db63df7e5c0a6340f410909e796
GreyLeaf23 <christian.torres2894@gmail.com>
2023-10-20 20:25:55 -0400

    Module adds arguments to Python list then saves it to a file,

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module that adds all arguments to a Python list, and
+then save them to a file."""
+
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    my_list = load_from_json_file(filename)
+except FileNotFoundError:
+    my_list = []
+
+    save_to_json_file(my_list + argv[1:], filename)
