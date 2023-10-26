71bca1bc3095f04aa5783303fd33e247e7f156c3
GreyLeaf23 <christian.torres2894@gmail.com>
2023-10-26 18:31:56 -0400

    Testing.

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -4,9 +4,7 @@ Module defines a class named Base and works as the foundation for the project.
 """
 
 
-from os import path
 import json
-import csv
 
 
 class Base:
@@ -22,52 +20,52 @@ class Base:
             self.id = Base.__nb_objects
 
 
-@staticmethod
-def to_json_string(list_dictionaries):
-    """Returns JSON string representation of list dictionaries"""
-    if list_dictionaries is None or len(list_dictionaries) == 0:
-        return "[]"
-    return json.dumps(list_dictionaries)
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Returns JSON string representation of list dictionaries"""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        return json.dumps(list_dictionaries)
 
 
-@classmethod
-def save_to_file(cls, list_objs):
-    """Converts JSON string to a regular file."""
-    objs = []
-    if list_objs is not None:
-        for obj in list_objs:
-            objs.append(cls.to_dictionary(obj))
-    filename = cls.__name__ + ".json"
-    with open(filename, mode="w") as f:
-        f.write(cls.to_json_string(objs))
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Converts JSON string to a regular file."""
+        objs = []
+        if list_objs is not None:
+            for obj in list_objs:
+                objs.append(cls.to_dictionary(obj))
+        filename = cls.__name__ + ".json"
+        with open(filename, mode="w") as f:
+            f.write(cls.to_json_string(objs))
 
 
-@staticmethod
-def from_json_string(json_string):
-    """Returns list of JSON string representing list of dictionaries."""
-    if json_string is None or len(json_string) == 0:
-        return []
-    return json.loads(json_string)
+    @staticmethod
+    def from_json_string(json_string):
+        """Returns list of JSON string representing list of dictionaries."""
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
 
 
-@classmethod
-def create(cls, **dictionary):
-    """Returns an instance with all attributes set."""
-    if cls.__name__ == "Rectangle":
-        dummy = cls(1, 1, 0, 0)
-    elif cls.__name__ == "Square":
-        dummy = cls(1, 0, 0)
-    dummy.update(**dictionary)
-    return dummy
+    @classmethod
+    def create(cls, **dictionary):
+        """Returns an instance with all attributes set."""
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1, 0, 0)
+        elif cls.__name__ == "Square":
+            dummy = cls(1, 0, 0)
+        dummy.update(**dictionary)
+        return dummy
 
 
-@classmethod
-def load_from_file(cls):
-    """Returns a list of instances."""
-    filename = cls.__name__ + ".json"
-    try:
-        with open(filename, mode="r") as f:
-            objs = cls.from_json_string(f.read())
-            return [cls.create(**obj) for obj in objs]
-    except FileNotFoundError:
-        return []
+    @classmethod
+    def load_from_file(cls):
+        """Returns a list of instances."""
+        filename = cls.__name__ + ".json"
+        try:
+            with open(filename, mode="r") as f:
+                objs = cls.from_json_string(f.read())
+                return [cls.create(**obj) for obj in objs]
+        except FileNotFoundError:
+            return []
