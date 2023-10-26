5f41d5400a8d2b5febe8d994f0a2abef429eba0f
GreyLeaf23 <christian.torres2894@gmail.com>
2023-10-26 09:27:52 -0400

    Tweak.

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -8,14 +8,13 @@ from models.base import Base
 class Rectangle(Base):
     """Creating class Rectangle with its dimensional elements."""
 
-
-def __init__(self, width, height, x=0, y=0, id=None):
-    """Dimensional elements of Rcetangle."""
-    super().__init__(id)
-    self.width = width
-    self.height = height
-    self.x = x
-    self.y = y
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Dimensional elements of Rcetangle."""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
 
 @property
