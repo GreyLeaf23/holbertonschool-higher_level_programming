5035f0e0111e16a57260a0a15dd21bb2f070be29
GreyLeaf23 <christian.torres2894@gmail.com>
2023-10-25 21:44:19 -0400

    Base file and Square file to its completion.

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@ Module defines a class named Base and works as the foundation for the project.
 
 
 from os import path
+import json
 
 
 class Base():
@@ -19,3 +20,54 @@ def __init__(self, id=None):
     else:
         Base.__nb_objects += 1
         self.id = Base.__nb_objects
+
+
+@staticmethod
+def to_json_string(list_dictionaries):
+    """Returns JSON string representation of list dictionaries"""
+    if list_dictionaries is None or len(list_dictionaries) == 0:
+        return "[]"
+    return json.dumps(list_dictionaries)
+
+
+@classmethod
+def save_to_file(cls, list_objs):
+    """Converts JSON string to a regular file."""
+    objs = []
+    if list_objs is not None:
+        for obj in list_objs:
+            objs.append(cls.to_dictionary(obj))
+    filename = cls.__name__ + ".json"
+    with open(filename, mode="w") as f:
+        f.write(cls.to_json_string(objs))
+
+
+@staticmethod
+def from_json_string(json_string):
+    """Returns list of JSON string representing list of dictionaries."""
+    if json_string is None or len(json_string) == 0:
+        return []
+    return json.loads(json_string)
+
+
+@classmethod
+def create(cls, **dictionary):
+    """Returns an instance with all attributes set."""
+    if cls.__name__ == "Rectangle":
+        dummy = cls(1, 1, 0, 0)
+    elif cls.__name__ == "Square":
+        dummy = cls(1, 0, 0)
+    dummy.update(**dictionary)
+    return dummy
+
+
+@classmethod
+def load_from_file(cls):
+    """Returns a list of instances."""
+    filename = cls.__name__ + ".json"
+    try:
+        with open(filename, mode="r") as f:
+            objs = cls.from_json_string(f.read())
+            return [cls.create(**obj) for obj in objs]
+    except FileNotFoundError:
+        return []
---
+++ python-almost_a_circle/models/square.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+"""Module creates class named Square"""
+
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class named Square that inherits aspects of Rectangle Class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Cretes dimensions of the Square."""
+    super().__init__(size, size, x, y, id)
+    self.size = size
+
+
+@property
+def size(self):
+    """Gets instance attribute - Size"""
+    return self.width
+
+
+@size.setter
+def size(self, value):
+    """
+    Sets instance attribute - Size.
+    Uses Width and Height to set it.
+    """
+    if type(value) is not int:
+        raise TypeError("width must be an intger")
+    if value <= 0:
+        raise ValueError("width must be > 0")
+    self.width = value
+    self.height = value
+
+
+def __str__(self):
+    """
+    Overrides method so it returns specified value.
+    [Square] (<id>) <x>/<y> - <size>
+    """
+    return "[{}] ({}) {}/{} - {}/{}".format(
+        self.__class_.__name__, self.id, self.x, self.y,
+        self.size)
+
+
+def update(self, *args, **kwargs):
+    """Assigns attributes to arguments."""
+    if args:
+        attr_list = ["id", "size", "x", "y"]
+        for item in range (len(args)):
+            setattr(self, attr_list[item], args[item])
+    else:
+        for key, value in kwargs.items():
+            setattr(self, key, value)
+
+
+def to_dictionary(self):
+    """Returns Square as dictionary."""
+    attr_list = ["id", "size", "x", "y"]
+    return {key: getattr(self, key) for key in attr_list}
+
