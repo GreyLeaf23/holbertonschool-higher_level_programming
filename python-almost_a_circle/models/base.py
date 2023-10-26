8f1dd111ecce38732c6f5c212eea696f24f19a63
GreyLeaf23 <christian.torres2894@gmail.com>
2023-10-26 09:22:22 -0400

    Gives output for Task 0.

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -9,18 +9,18 @@ import json
 import csv
 
 
-class Base():
+class Base:
     """Creation class Base"""
     __nb_objects = 0
 
 
-def __init__(self, id=None):
-    """Method initializes objects with an id."""
-    if id is not None:
-        self.id = id
-    else:
-        Base.__nb_objects += 1
-        self.id = Base.__nb_objects
+    def __init__(self, id=None):
+        """Method initializes objects with an id."""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
 
 
 @staticmethod
