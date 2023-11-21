b08a1772a2bade08fd3e3acc55de40493e26221c
GreyLeaf23 <christian.torres2894@gmail.com>
2023-11-20 21:32:36 -0400

    Script shows all records of 'score' and 'name' from second_table.

--- SQL_introduction/11-best_score.sql
+++ SQL_introduction/11-best_score.sql
@@ -1,2 +1,5 @@
 -- Shows all records of 'score' and 'name' columns with 'score >= 10' from 'second_table' table.
-SELECT `score`, `name` FROM `second_table` WHERE 'score' >= 10 ORDER BY `score` DESC;
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
+ORDER BY `score` DESC;
