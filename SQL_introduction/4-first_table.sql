af0fc06a7ce810bd64c0fd5ad9b075ba19f02927
GreyLeaf23 <christian.torres2894@gmail.com>
2023-11-20 20:53:57 -0400

    Maybe?

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,3 +1,3 @@
--- Creates the specified table with descriptors.
--- Create table if doesn't exist.
-CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT,'name' VARCHAR(256));
+-- Makes a table with a id and name description the name will be
+-- received and it will not fail if the table already exists
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
