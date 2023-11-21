e7a0feff0a3346197661b41b614402fc65d02795
GreyLeaf23 <christian.torres2894@gmail.com>
2023-11-20 21:33:34 -0400

    Better code organization.

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,4 @@
 -- Shows all records of 'score' and 'name' columns from 'second_table' table.
-SELECT `score`, `name` FROM `second_table` ORDER BY `score` DESC;
+SELECT `score`, `name`
+FROM `second_table`
+ORDER BY `score` DESC;
