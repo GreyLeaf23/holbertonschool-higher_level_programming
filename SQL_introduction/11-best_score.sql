e32d937ebb7edf0e33e68a69e5f635ceb8ef17de
GreyLeaf23 <christian.torres2894@gmail.com>
2023-11-20 21:29:13 -0400

    Shows all records of second_table with 'score >= 10'.

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- Shows all records of 'score' and 'name' columns with 'score >= 10' from 'second_table' table.
+SELECT `score`, `name` FROM `second_table`, WHERE 'score' >= 10, ORDER BY `score` DESC;
