dd70457607940b8bf7ef3d770074874a7e3fb4c9
GreyLeaf23 <christian.torres2894@gmail.com>
2023-11-20 21:20:28 -0400

    Script creates a new table called 'second_table' with records.

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,6 @@
--- Create a second table called 'second_table' with records.
-CREATE TABLE IF NOT EXISTS 'second_table' ('id' INT, 'name' VARCHAR(256), 'score' INT);
-INSERT INTO 'second_table' ('id', 'name', 'score') VALUES (1, "John", 10);
-INSERT INTO 'second_table' ('id', 'name', 'score') VALUES (2, "Alex", 3);
-INSERT INTO 'second_table' ('id', 'name', 'score') VALUES (3, "Bob", 14);
-INSERT INTO 'second_table' ('id', 'name', 'score') VALUES (4, "George", 8);
+-- creates a table second_table in the database and add multiples rows.
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
