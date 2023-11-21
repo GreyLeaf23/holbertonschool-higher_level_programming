a5afc7e2b8cbeed4f1e5049f685ddab5b2fbc406
GreyLeaf23 <christian.torres2894@gmail.com>
2023-11-20 20:52:16 -0400

    Script creates specified table with descriptors if doesn't exist.

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,6 +1,3 @@
 -- Creates the specified table with descriptors.
 -- Create table if doesn't exist.
-CREATE TABLE IF NOT EXISTS 'first_table' (
-    'id' INT,
-    'name' VARCHAR(256)
-);
+CREATE TABLE IF NOT EXISTS 'first_table' ('id' INT,'name' VARCHAR(256));
