39e453f1aec9613908127eba46360c3b275e2aff
GreyLeaf23 <christian.torres2894@gmail.com>
2023-11-22 16:58:22 -0400

    Script takes user input and displays data that matches.

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Module to take user input and display all values in the states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """Function that takes user input and displays all values in the states table of hbtn_0e_0_usa where name matches the argument."""
+
+    # Connecting to a MySQL database.
+    cnx = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                          user=sys.argv[1], passwd=sys.argv[2], database=sys.argv[3])
+
+    # Making cursor obj for execution.
+    cur = cnx.cursor()
+
+    # Executing query.
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(sys.argv[4]))
+
+    # Obtaining query results.
+    query_rows = cur.fetchall()
+
+    # Printing results.
+    for row in query_rows:
+        if row[1][0] == sys.argv[4][0]:
+            print(row)
+
+    # Close cursor.
+    cur.close()
+
+    # Close connection to database.
+    cnx.close()
