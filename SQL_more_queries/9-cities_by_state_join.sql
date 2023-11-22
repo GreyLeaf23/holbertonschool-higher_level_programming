6ae8336bad994edec9357797e699572f1069a29d
GreyLeaf23 <christian.torres2894@gmail.com>
2023-11-21 21:16:34 -0400

    Script lists all cities with specified records.

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Show all cities contained in the database hbtn_0d_usa
+-- On each avaivable record, display: cities.id - cities.name - states.name
+SELECT c.`id`, c.`name`, s.`name`
+FROM `cities` AS c
+    INNER JOIN `states` AS s
+    ON c.`state_id` = s.`id`
+ORDER BY c.`id`;
