bd82c6d2213d5827dcfe2437722d5b9d1fc95292
GreyLeaf23 <christian.torres2894@gmail.com>
2024-01-31 09:55:18 -0400

    Script updates text color of header to red when an html element is click upon.

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,9 @@
+// Select the header element.
+const red_header = document.getElementById('red_header');
+
+// Listen for a click event on the header element.
+red_header.addEventListener('click', function() {
+    // Change the header's text color to red.
+    const red_header = document.querySelector('header');
+    red_header.style.color = '#FF0000';
+    });
